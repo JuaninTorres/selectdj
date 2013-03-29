c28a84116461eeccff69b4ac48bc1464fb7ff265
Juan Torres <juanin.torres@gmail.com>
2013-03-29 14:51:49 -0300

    Agregado un ejemplo de archivo de configuracion

---
+++ configuration.inc.sample.php
@@ -0,0 +1,10 @@
+<?php
+    $connectPDO_server = 'localhost';
+    $connectPDO_port   = '3306';
+    $connectPDO_db     = 'db_name';
+    $connectPDO_user   = 'db_user';
+    $connectPDO_pass   = 'db_password';
+
+    define('DOCUMMENT_ROOT',$_SERVER['DOCUMENT_ROOT']);
+    define('CLAVEMAESTRA','algunaclavemaestraquesemeocurra');
+?>
